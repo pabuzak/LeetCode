6bd15b8b7b156a6ed8b2c48653ad2573aa90a280
pabuzak <pabuzak@gmail.com>
2024-07-01 00:00:58 +1200

    443 revisit later

---
+++ 443.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def compress(self, chars):
+        r, i = 0, 0
+
+        while(i < len(chars)):
+            currChar = chars[i]
+            currOcc = 0
+            while((i<len(chars)) and (chars[i] == currChar)):
+                currOcc += 1
+                i += 1
+            
+            chars[r] = currChar
+            r += 1
+
+            if(currOcc > 1):
+                currOccStr = str(currOcc)
+                for j in range(len(currOccStr)):
+                    chars[r] = currOccStr[j]
+                    r += 1
+
+        return r
+            
+
+chars = ["a","b","c"]
+s = Solution()
+print(s.compress(chars))
+print(chars)
