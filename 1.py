d845f9f6d6cf7581392896e030210471f6af4545
pabuzak <pabuzak@gmail.com>
2024-07-05 00:48:49 +1200

    49 not finished, only done base cases, revist

--- 1.py
+++ 1.py
@@ -22,3 +22,15 @@ if not output:
         length -= 1
 
 print(list(output))
+
+
+
+### optimised code
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         numToIndex = {}
+#         for i in range(len(nums)):
+#             if target - nums[i] in numToIndex:
+#                 return [numToIndex[target - nums[i]], i]
+#             numToIndex[nums[i]] = i
+#         return []
