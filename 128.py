4c04af5c8fd0daac2d27da70c577f464eff4c5b5
pabuzak <pabuzak@gmail.com>
2024-08-03 12:35:02 +1200

    not completed 128

--- 128.py
+++ 128.py
@@ -1,13 +1,19 @@
-nums=[0,-1]
+nums=[9,1,4,7,3,-1,0,5,8,-1,6]
 
-
-result = 0
 seq = set(nums)
 num = 0
+max_count = 0
 
 for n in seq:
-    if n-1 in seq:
-        print(n)
+    if n-1 not in seq:
+        count = 1
+        num = n
+        for n in seq:
+            if num+1 in seq:
+                count += 1
+            else:
+                break
 
+        max_count = max(count, max_count)
 
-print(result)
+print(max_count)
