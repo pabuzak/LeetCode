86cb8b4bc64e2da6d7062e217a9328d6c4ba50ea
pbuzak <16011@my.sanctamaria.school.nz>
2024-08-03 23:51:34 +1200

    stuck on 11, try draw w/ ipad

--- 11.py
+++ 11.py
@@ -1,29 +1,19 @@
-heights=[1,7,2,5,12,3,500,500,7,8,4,7,3,6]
+heights=[1,2,4,3]
 
 l = 0
-r = len(heights)-1
-num = 0
+r = len(heights)
 result = 0
-length = 0
+num = 0
+    
 
 while l < r:
-    if heights[l] >= num:
-        num = heights[l]
-        temp = 0
-        for n in range(r, l-1, -1):
-            print(heights[n])
-            if heights[n] > num:
-                pass
-            else:
-                temp_length = n-l
-                if temp_length > length:
-                    length = temp_length
-                    result = heights[n] * length
-                    result = max(result,temp)
-    l += 1
-            
-
-        
+    num = heights[l]
+    for n in range(l, r):
+        if heights[n] > num:
+            pass
+        else:
+            length = n-l
+            result = max(result,heights[n] * length)
     l += 1
 
 print(result)
