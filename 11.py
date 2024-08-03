d8f463501ea0330b3fbe0d2bf3cb40d81ea2b8d1
pbuzak <16011@my.sanctamaria.school.nz>
2024-08-03 17:20:16 +1200

    11 attempt

--- 11.py
+++ 11.py
@@ -1,27 +1,29 @@
-height=[1,7,2,5,12,3,500,500,7,8,4,7,3,6]
+heights=[1,7,2,5,12,3,500,500,7,8,4,7,3,6]
 
 l = 0
-r = len(height)-1
+r = len(heights)-1
 num = 0
 result = 0
 length = 0
 
 while l < r:
-    if height[l] >= num:
-        num = height[l]
+    if heights[l] >= num:
+        num = heights[l]
         temp = 0
-        temp_length = 0
         for n in range(r, l-1, -1):
-            print(r, l)
-            print(n)
-            if height[n] > num:
+            print(heights[n])
+            if heights[n] > num:
                 pass
             else:
-                temp_length = max(temp_length, n-l)
+                temp_length = n-l
                 if temp_length > length:
                     length = temp_length
-                    result = height[n] * height[n]
+                    result = heights[n] * length
                     result = max(result,temp)
+    l += 1
+            
+
+        
     l += 1
 
 print(result)
